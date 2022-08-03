b52a21033b38bec1275a7c1c091740afe180faa8
Gatwaza <gatwazarobert177@gmail.com>
2022-08-03 12:15:18 -0700

    Putting white feathers together

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,9 +1,5 @@
--- List all records of 'second_table' of db 'hbtn_0c_0'
--- Don't list rows without a 'name' value
--- Results should display score and name
--- Records should be listed descending score
--- db name will be passed as arg to mysql cmd
--- Not allowed to use JOIN or UNION
+-- Lists the number of records with the ame score in the table 'second_table'
+-- from the database hbtn_0c_0
 SELECT score;
 COUNT(*) as number
 FROM second_table
