2204fd03d0d9e1b5de50578fee2dcba079e921bf
Jean Robert Gatwaza <gatwazarobert177@gmail.com>
2022-10-03 04:35:55 -0700

    So, vertical descending right?

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//Backslash &  n(\n and +) helps us print out line in vertical descending.
+let language = 'C is fun\n' +
+     'python is cool\n' +
+     'JavaScript is amazing'
+console.log(language);
